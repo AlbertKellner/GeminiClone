1fc2d6f0e84dd7c7891f85840c97be4948974852
Albert Kellner Lescinskiene <albert.kellner@gmail.com>
2023-05-02 00:25:25 -0300

    checkpoint

--- ArquivosDoDisco.Web/Controllers/StructureController.cs
+++ ArquivosDoDisco.Web/Controllers/StructureController.cs
@@ -1,4 +1,5 @@
-﻿using ArquivosDoDisco.UseCase;
+﻿using ArquivosDoDisco.Entities;
+using ArquivosDoDisco.UseCase;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using System.Text.Json;
@@ -24,7 +25,7 @@ namespace ArquivosDoDisco.Web.Controllers
                 {
                     WriteIndented = false
                 };
-                var jsonString = System.Text.Json.JsonSerializer.Serialize(driveToScan, jsonOptions);
+                var jsonString = JsonSerializer.Serialize(driveToScan, jsonOptions);
                 return Content(jsonString, "application/json");
             }
             else
@@ -36,14 +37,13 @@ namespace ArquivosDoDisco.Web.Controllers
         [HttpGet("{selectedDrive}")]
         [ProducesResponseType((int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.NoContent)]
-        public async Task<IActionResult> GetByDiskAsync(
-            [FromRoute]string selectedDrive)
+        public async Task<IActionResult> GetAllByDiskAsync(
+            [FromRoute] string selectedDrive)
         {
-            //var driveToScan = DriverFind.GetDrive();
+            Entities.MyDiskItemEntity structure = await FileManager.ListFoldersAndFilesAsync($"{selectedDrive}:/");
 
-            var structure = await FileManager.ListFoldersAndFilesAsync(selectedDrive);
-
-            RemoveNonFoldersRecursively(structure);
+            //RemoveNonFoldersRecursively(structure);
+            RemoveFoldersRecursively(structure, 0);
 
             if (structure != null)
             {
@@ -51,7 +51,7 @@ namespace ArquivosDoDisco.Web.Controllers
                 {
                     WriteIndented = false
                 };
-                var jsonString = System.Text.Json.JsonSerializer.Serialize(structure, jsonOptions);
+                var jsonString = JsonSerializer.Serialize(structure, jsonOptions);
                 return Content(jsonString, "application/json");
             }
             else
@@ -59,6 +59,37 @@ namespace ArquivosDoDisco.Web.Controllers
                 return NoContent();
             }
         }
+
+        [HttpGet("{selectedDrive}/folder/{selectedFolder}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        public async Task<IActionResult> GetByFolderAsync(
+            [FromRoute] string selectedDrive,
+            [FromRoute] string selectedFolder)
+        {
+            selectedDrive = $"{selectedDrive}:/";
+
+            var structure = await FileManager.ListFoldersAndFilesAsync(selectedDrive);
+
+            var result = DriverFind.FindFolder(structure, selectedFolder);
+
+            //RemoveNonFoldersRecursively(structure);
+
+            if (result != null)
+            {
+                var jsonOptions = new JsonSerializerOptions
+                {
+                    WriteIndented = false
+                };
+                var jsonString = JsonSerializer.Serialize(result, jsonOptions);
+                return Content(jsonString, "application/json");
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+
         private void RemoveNonFoldersRecursively(Entities.MyDiskItemEntity item)
         {
             if (item.Children == null)
@@ -72,5 +103,33 @@ namespace ArquivosDoDisco.Web.Controllers
                 RemoveNonFoldersRecursively(child);
             }
         }
+
+        private void RemoveFoldersRecursively(MyDiskItemEntity folder, int depth = 0)
+        {
+            if (folder.Children == null)
+            {
+                return;
+            }
+
+            if (depth > 4)
+            {
+                folder.Children.Clear();
+                return;
+            }
+
+            for (int i = folder.Children.Count - 1; i >= 0; i--)
+            {
+                MyDiskItemEntity child = folder.Children[i];
+                if (child.IsFolder)
+                {
+                    RemoveFoldersRecursively(child, depth + 1);
+                }
+                else
+                {
+                    folder.Children.RemoveAt(i);
+                }
+            }
+        }
+
     }
 }
